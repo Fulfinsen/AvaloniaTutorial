236702cf88446e46a2fa8b7c7bc308a7607e6692
Fulfinsen <dincasilviu2@gmail.com>
2026-02-20 18:39:43 +0200

    temperature converter

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,4 +1,7 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using System.Diagnostics;
+using Avalonia;
 
 namespace BatchProcess3;
 
@@ -7,5 +10,23 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+    
+    private void CelsiusChanged_OnTextChanged(object? sender, TextChangedEventArgs e)
+    {
+        if (string.IsNullOrEmpty(Celsius.Text) || Celsius.Text == "-")
+        {
+            Fahrenheit.Text = "";
+        }
+        else if (double.TryParse(Celsius.Text, out double C))
+        {
+            var F = C * (9d / 5d) + 32;
+            Fahrenheit.Text = F.ToString("0.0");
+        }
+        else
+        {
+            Celsius.Text = "0";
+            Fahrenheit.Text = "0";
+        }
     }
 }
